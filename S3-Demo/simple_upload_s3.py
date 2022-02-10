f5cfbbb5edc03ea3a6d60dbaec5b601afd4d443b
Jihad Banna <super_banna@hotmail.com>
2022-02-10 22:44:17 +0200

    terraf

--- S3-Demo/simple_upload_s3.py
+++ S3-Demo/simple_upload_s3.py
@@ -1,3 +1,3 @@
 import boto3
 s3_client = boto3.client('s3')
-s3_client.upload_file('../changeMe.txt', 'testbuckeralonit3', 'changeMe.txt')
+s3_client.upload_file('../README.md', 'jibigbucket', 'gold.py')
